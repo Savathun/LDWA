24c73f998d0e23655d80742866e3a48bf493a980
Savathun <djschlatt@gmail.com>
2021-05-27 19:46:13 -0600

    add function to extract manifest zip file

--- main.py
+++ main.py
@@ -4,21 +4,33 @@ def retrieve_manifest(manifest_location):
     urllib.request.urlretrieve('https://www.bungie.net' + manifest_location, 'manifest\\manifest.zip')
 
 
+def extract_manifest():
+    from zipfile import ZipFile
+    with ZipFile('manifest\\manifest.zip', 'r') as zipObj:
+        zipObj.infolist()[0].filename = 'manifest.sqlite3'
+        zipObj.extract(zipObj.infolist()[0], path='manifest')
+
+
 def check_manifest_updates():
     import requests
-    with open('api-key.txt', "r") as file:
+    with open('api_key.txt', "r") as file:
         api_key = file.read()
-    manifest_version = open('manifest_version.txt', "w+")
+    file.close()
+    manifest_version = open('manifest\\manifest_version.txt', "w+")
     HEADERS = {"X-API-Key": api_key}
     r = requests.get("https://www.bungie.net/platform/Destiny2/Manifest/", headers=HEADERS)
     manifest_location = r.json()['Response']['mobileWorldContentPaths']['en']
     if manifest_version.read() != manifest_location.split('/')[-1][18:-8]:
         manifest_version.write(manifest_location.split('/')[-1][18:-8])
         retrieve_manifest(manifest_location)
+        extract_manifest()
+    manifest_version.close()
+
 
 def main():
     """ """
     check_manifest_updates()
+
 
 if __name__ == '__main__':
     main()
