6c1e1c6900345aaf8a094e9e625e51b7b5a9ec22
Savathun <djschlatt@gmail.com>
2021-05-27 19:23:07 -0600

    Seperate main function into seperate functions

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-def main():
+def retrieve_manifest(manifest_location):
     """ """
-    import requests
     import urllib.request
-    import pprint
-    # dictionary to hold extra headers
+    urllib.request.urlretrieve('https://www.bungie.net' + manifest_location, 'manifest\\manifest.zip')
+
+
+def check_manifest_updates():
+    import requests
     with open('api-key.txt', "r") as file:
         api_key = file.read()
+    manifest_version = open('manifest_version.txt', "w+")
     HEADERS = {"X-API-Key": api_key}
     r = requests.get("https://www.bungie.net/platform/Destiny2/Manifest/", headers=HEADERS)
     manifest_location = r.json()['Response']['mobileWorldContentPaths']['en']
-    manifest_url = 'https://www.bungie.net' + manifest_location
-    urllib.request.urlretrieve(manifest_url, manifest_location.split('/')[-1] + '.zip')
+    if manifest_version.read() != manifest_location.split('/')[-1][18:-8]:
+        manifest_version.write(manifest_location.split('/')[-1][18:-8])
+        retrieve_manifest(manifest_location)
+
+def main():
+    """ """
+    check_manifest_updates()
+
 if __name__ == '__main__':
     main()
