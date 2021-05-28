5511d4b9af273a5277dc8189977e48218f5d38a8
Savathun <djschlatt@gmail.com>
2021-05-28 01:43:44 -0600

    Update main to take the database and produce a narrowed down csv from it

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import json
+
+
 def retrieve_manifest(manifest_location):
     """ """
     import urllib.request
@@ -7,7 +10,7 @@ def retrieve_manifest(manifest_location):
 def extract_manifest():
     from zipfile import ZipFile
     with ZipFile('manifest\\manifest.zip', 'r') as zipObj:
-        zipObj.infolist()[0].filename = 'manifest.sqlite3'
+        zipObj.infolist()[0].filename = 'manifest.sqlite'
         zipObj.extract(zipObj.infolist()[0], path='manifest')
 
 
@@ -30,13 +33,38 @@ def check_manifest_updates():
 
 def connect_db():
     import dbclass
-    db = dbclass.Database('manifest\\manifest.sqlite3')
+    db = dbclass.Database('manifest\\manifest.sqlite')
+    return db
 
 
 def main():
-    """ """
     check_manifest_updates()
-    connect_db()
+    db = connect_db()
+    inventory_items = db.fetch("DestinyInventoryItemDefinition")
+    inventory_items = [[entry[0], json.loads(entry[1])] for entry in inventory_items]
+    import pandas as pd
+    import numpy as np
+
+    inventory_df = pd.json_normalize([entry[1] for entry in inventory_items])
+    inventory_df.index = [entry[0] for entry in inventory_items]
+    inventory_df = inventory_df[inventory_df['itemType'] == 3]
+    inventory_df = inventory_df[inventory_df['inventory.tierTypeName'] == 'Legendary']
+    inventory_df = inventory_df.explode('quality.versions')
+    inventory_df = pd.concat([inventory_df.drop(['quality.versions'], axis=1),
+                              inventory_df['quality.versions'].apply(pd.Series)], axis=1)
+    inventory_df = inventory_df[inventory_df['powerCapHash'] == 2759499571]
+    inventory_df = inventory_df.drop_duplicates(subset=['displayProperties.name'])
+    inventory_df = inventory_df.drop(inventory_df.std()[(inventory_df.std() == 0)].index, axis=1)
+    for column in inventory_df.columns:
+        try:
+            inventory_df[column] = inventory_df[column].apply(lambda y: np.nan if len(y) == 0 else y)
+            if len(set(inventory_df[column])) == 1:
+                inventory_df = inventory_df.drop([column], axis=1)
+        except TypeError:
+            pass
+    inventory_df = inventory_df.dropna(axis='columns', how='all')
+    inventory_df['flavorText'] = inventory_df['flavorText'].replace(r'\n', ' ', regex=True)
+    inventory_df.to_csv('manifest\\dataframe.csv', index=True)
 
 
 if __name__ == '__main__':
