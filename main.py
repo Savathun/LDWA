2ecb0b7f68515516f95ef16f68998cf924cc4180
Savathun <djschlatt@gmail.com>
2021-05-31 18:49:54 -0600

    improve style of manifest_updates function

--- main.py
+++ main.py
@@ -23,17 +23,12 @@ def extract_manifest():
 
 
 def check_manifest_updates():
-    import requests
-    with open('api_key.txt', "r") as file:
-        api_key = file.read()
-    file.close()
-    manifest_version = open('manifest\\manifest_version.txt', "r+")
-    headers = {"X-API-Key": api_key}
-    r = requests.get("https://www.bungie.net/platform/Destiny2/Manifest/", headers=headers)
-    manifest_location = r.json()['Response']['mobileWorldContentPaths']['en']
-    return (True, manifest_location) if manifest_version.read() != manifest_location.split('/')[-1][18:-8] else (False,
-                                                                                                                 0)
-
+    from requests import get
+    manifest_location = get("https://www.bungie.net/platform/Destiny2/Manifest/",
+                            headers={"X-API-Key": open('api_key.txt', "r").read()}).json()['Response'][
+        'mobileWorldContentPaths']['en']
+    return (True, manifest_location) if open('manifest\\manifest_version.txt',
+                                             "r+").read() != manifest_location.split('/')[-1][18:-8] else (False, 0)
 
 def retrieve_table(db, table_name):
     table_list = db.fetch(table_name)
