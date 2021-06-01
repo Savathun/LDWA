beea6387dde1f177b1a79635147fcb5633db4385
Savathun <djschlatt@gmail.com>
2021-05-31 18:57:16 -0600

    convert csvs and their generators to pickles, condense methods

--- main.py
+++ main.py
@@ -30,55 +30,47 @@ def check_manifest_updates():
     return (True, manifest_location) if open('manifest\\manifest_version.txt',
                                              "r+").read() != manifest_location.split('/')[-1][18:-8] else (False, 0)
 
-def retrieve_table(db, table_name):
-    table_list = db.fetch(table_name)
-    table_list = [[entry[0], json.loads(entry[1])] for entry in table_list]
-    return table_list
 
-
-def create_dataframe(table_list):
-    table_df = pd.json_normalize([entry[1] for entry in table_list])
+def generate_pickle(db, name, columns):
+    from pandas import json_normalize
+    from json import loads
+    table_list = [[entry[0], loads(entry[1])] for entry in db.fetch('Destiny' + name + "Definition")]
+    table_df = json_normalize([entry[1] for entry in table_list], sep='_')
     table_df.index = [entry[0] for entry in table_list]
-    return table_df
+    table_df[columns].to_pickle('manifest\\{}_dataframe.pkl'.format(name))
 
 
-def reduce_dataframe(inventory_df):
-    inventory_df = inventory_df[inventory_df['itemType'] == 3]
-    inventory_df = inventory_df[inventory_df['inventory.tierTypeName'] == 'Legendary']
-    inventory_df = inventory_df.explode('quality.versions')
-    inventory_df = pd.concat([inventory_df.drop(['quality.versions'], axis=1),
-                              inventory_df['quality.versions'].apply(pd.Series)], axis=1)
-    inventory_df = inventory_df[inventory_df['powerCapHash'] == 2759499571]
-    inventory_df = inventory_df.drop_duplicates(subset=['displayProperties.name'])
-    return inventory_df
+def generate_weapons_dataframe(inventory_df, perk_df, plug_sets_df, damage_type_df, slot_df):
+    def reduce_inventory_to_weapons(inventory_df):
+        def refine_socket_entries(inventory_df):
+            inventory_df[['sockets_socketEntries_{}'.format(x) for x in
+                          range(len(inventory_df.iloc[1]['sockets_socketEntries']))]] = DataFrame(
+                inventory_df['sockets_socketEntries'].values.tolist(),
+                index=inventory_df.index)
+            inventory_df['sockets_socketEntries_0_singleInitialItemHash'] = DataFrame(
+                inventory_df['sockets_socketEntries_0'].apply(lambda x: x['singleInitialItemHash']),
+                index=inventory_df.index)
+            inventory_df[['sockets_socketEntries_{}_randomizedPlugSetHash'.format(x) for x in range(1, 5)]] = DataFrame(
+                inventory_df[['sockets_socketEntries_{}'.format(x) for x in range(1, 5)]].apply(
+                    lambda x: x.apply(lambda y: y['randomizedPlugSetHash'] if 'randomizedPlugSetHash' in y else 0)),
+                index=inventory_df.index)
+            return inventory_df.drop(
+                ['sockets_socketEntries'] + ['sockets_socketEntries_{}'.format(x) for x in range(0, 10)], axis=1)
 
+        from pandas import concat, Series
+        inventory_df = inventory_df[(inventory_df['itemType'] == 3) &
+                                    (inventory_df['inventory_tierTypeName'] == 'Legendary')].explode('quality_versions')
+        inventory_df = concat([inventory_df.drop(['quality_versions'], axis=1),
+                               inventory_df['quality_versions'].apply(Series)], axis=1)
+        return refine_socket_entries(
+            inventory_df[inventory_df['powerCapHash'] == 2759499571].drop_duplicates(subset=['displayProperties_name'])[
+                ['itemTypeDisplayName', 'displayProperties_name', 'defaultDamageTypeHash',
+                 'equippingBlock_equipmentSlotTypeHash', 'equippingBlock_ammoType', 'sockets_socketEntries']])
 
-def select_needed_columns(inventory_df):
-    refined_df = inventory_df[
-        ['itemTypeDisplayName', 'displayProperties.name', 'defaultDamageTypeHash',
-         'equippingBlock.equipmentSlotTypeHash',
-         'equippingBlock.ammoType', 'sockets.socketEntries']].copy()
-    return refined_df
-
-
-def refine_socket_entries(refined_df):
-    refined_df[['sockets.socketEntries.{}'.format(x) for x in
-                range(len(refined_df.iloc[1]['sockets.socketEntries']))]] = pd.DataFrame(
-        refined_df['sockets.socketEntries'].values.tolist(),
-        index=refined_df.index)
-    refined_df['sockets.socketEntries.0.singleInitialItemHash'] = pd.DataFrame(
-        refined_df['sockets.socketEntries.0'].apply(lambda x: x['singleInitialItemHash']), index=refined_df.index)
-    refined_df[['sockets.socketEntries.{}.randomizedPlugSetHash'.format(x) for x in range(1, 5)]] = pd.DataFrame(
-        refined_df[['sockets.socketEntries.{}'.format(x) for x in range(1, 5)]].apply(
-            lambda x: x.apply(lambda y: y['randomizedPlugSetHash'] if 'randomizedPlugSetHash' in y else 0)),
-        index=refined_df.index)
-    refined_df = refined_df.drop(
-        ['sockets.socketEntries'] + ['sockets.socketEntries.{}'.format(x) for x in range(0, 10)], axis=1)
-    return refined_df
-
-
-def convert_hashes(weapons_df, perk_df, plug_sets_df, damage_type_df, slot_df):
-    weapons_df['defaultDamageTypeHash'] = pd.DataFrame(
+    from pandas import DataFrame
+    ammo_type_list = ['None', 'Primary', 'Special', 'Heavy', 'Unknown']
+    weapons_df = reduce_inventory_to_weapons(inventory_df)
+    weapons_df['defaultDamageTypeHash'] = DataFrame(
         weapons_df['defaultDamageTypeHash'].apply(
             lambda x: damage_type_df.loc[damage_type_df['hash'] == x]['displayProperties_name'].values[0]),
         index=weapons_df.index)
@@ -108,41 +100,39 @@ def convert_hashes(weapons_df, perk_df, plug_sets_df, damage_type_df, slot_df):
 def main():
     update_needed, location = check_manifest_updates()
     if update_needed:
-        manifest_version = open('manifest\\manifest_version.txt', "r+")
-        manifest_version.seek(0)
-        manifest_version.write(location.split('/')[-1][18:-8])
+        open('manifest\\manifest_version.txt', "r+").write(location.split('/')[-1][18:-8])
         retrieve_manifest(location)
     from os.path import exists
     if not exists('manifest\\manifest.sqlite') or update_needed:
         extract_manifest()
-    if not os.path.exists('manifest\\weapons_dataframe.csv') or update_needed:
-        import dbclass
-        db = dbclass.Database('manifest\\manifest.sqlite')
-        inventory_df = create_dataframe(retrieve_table(db, "DestinyInventoryItemDefinition"))
-        perk_df = \
+    db = None
+    for name, columns in zip(['InventoryItem', 'DamageType', 'EquipmentSlot', 'PlugSet'],
+                             [['hash', 'itemCategoryHashes', 'itemTypeDisplayName', 'displayProperties_name',
+                               'itemType', 'equippingBlock_equipmentSlotTypeHash', 'inventory_tierTypeName',
+                               'quality_versions', 'equippingBlock_ammoType', 'sockets_socketEntries',
+                               'defaultDamageTypeHash'], ['hash', 'displayProperties_name'],
+                              ['hash', 'displayProperties_name'], ['hash', 'reusablePlugItems']]):
+        if not exists('manifest\\{}_dataframe.pkl'.format(name)) or update_needed:
+            db = Database('manifest\\manifest.sqlite') if not None else db
+            generate_pickle(db, name, columns)
+    if not exists('manifest\\weapons_dataframe.pkl') or update_needed:
+        from pandas import read_pickle
+        inventory_df = read_pickle('manifest\\InventoryItem_dataframe.pkl')
+        if not exists('manifest\\perk_dataframe.pkl') or update_needed:
             inventory_df[
                 inventory_df['itemCategoryHashes'].apply(lambda x: 610365472 in x if type(x) is list else False)][
-                ['hash', 'displayProperties.name']]
-        perk_df.to_csv('manifest\\perk_dataframe.csv', index=True)
-        damage_type_df = create_dataframe(retrieve_table(db, "DestinyDamageTypeDefinition"))[
-            ['hash', 'displayProperties.name']]
-        damage_type_df.to_csv('manifest\\damage_type_dataframe.csv', index=True)
-        slot_df = create_dataframe(retrieve_table(db, "DestinyEquipmentSlotDefinition"))[
-            ['hash', 'displayProperties.name']]
-        slot_df.to_csv('manifest\\slot_dataframe.csv', index=True)
-        plug_sets_df = create_dataframe(retrieve_table(db, "DestinyPlugSetDefinition"))[['hash', 'reusablePlugItems']]
-        plug_sets_df.to_csv('manifest\\plug_sets_dataframe.csv', index=True)
-        weapons_df = reduce_dataframe(inventory_df)
-        weapons_df = select_needed_columns(weapons_df)
-        weapons_df = refine_socket_entries(weapons_df)
-        weapons_df = convert_hashes(weapons_df, perk_df, plug_sets_df, damage_type_df, slot_df)
-        weapons_df.to_csv('manifest\\weapons_dataframe.csv', index=True)
-
-    # weapons_df = pd.read_csv('manifest\\weapons_dataframe.csv', index_col=0)
-    # perk_df = pd.read_csv('manifest\\perk_dataframe.csv', index_col=0)
-    # plug_sets_df = pd.read_csv('manifest\\plug_sets_dataframe.csv', index_col=0)
-    # slot_df = pd.read_csv('manifest\\slot_dataframe.csv', index_col=0)
-    # damage_type_df = pd.read_csv('manifest\\damage_type_dataframe.csv', index_col=0)
+                ['hash', 'displayProperties_name']].to_pickle('manifest\\perk_dataframe.pkl')
+        if not exists('manifest\\weapons_dataframe.pkl') or update_needed:
+            generate_weapons_dataframe(inventory_df,
+                                       read_pickle('manifest\\perk_dataframe.pkl'),
+                                       read_pickle('manifest\\PlugSet_dataframe.pkl'),
+                                       read_pickle('manifest\\DamageType_dataframe.pkl'),
+                                       read_pickle('manifest\\EquipmentSlot_dataframe.pkl')
+                                       ).to_pickle('manifest\\weapons_dataframe.pkl')
+    from pandas import read_pickle
+    weapons_df = read_pickle('manifest\\weapons_dataframe.pkl')
+    weapons_df.columns = ['Type', 'Name', 'Element', 'Slot', 'Ammo', 'Archetype',
+                          'perk_column_1', 'perk_column_2', 'perk_column_3', 'perk_column_4']
 
 
 if __name__ == '__main__':
