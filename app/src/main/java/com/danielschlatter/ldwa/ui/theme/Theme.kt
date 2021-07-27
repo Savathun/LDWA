4d3ca5c82d5fe155af9ca3bcfe22b35443e2aa05
Savathun <djschlatt@gmail.com>
2021-07-27 08:34:05 -0600

    fix minor errors

--- app/src/main/java/com/danielschlatter/ldwa/ui/theme/Theme.kt
+++ app/src/main/java/com/danielschlatter/ldwa/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun LDWATheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun LDWATheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
